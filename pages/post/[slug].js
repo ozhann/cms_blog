3895495f14d8659b652e51ed7fc78e79412441c5
ozhann <ozhanyilmaz@gmail.com>
2021-12-05 20:31:48 +0100

    add category pages

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import {getPosts, getPostDetails} from '../../services'
-import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
 
 const PostDetails = ({post}) => {
+    const router = useRouter()
+
+    if(router.isFallback) {
+        return <Loader />
+    }
     return (
         <div className='container px-10 mx-auto mb-8'>
             <div className='grid grid-cols-1 gap-12 lg:grid-cols-12'>
