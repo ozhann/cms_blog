ff960f2b8f7aae03713ffeefd3b324c1c286f46d
ozhann <ozhanyilmaz@gmail.com>
2021-12-05 20:40:28 +0100

    add getCategoryPost

--- services/index.js
+++ services/index.js
@@ -158,3 +158,42 @@ export const getComments = async({ slug }) => {
 
     return result.comments;
 }
+
+
+export const getCategoryPost = async (slug) => {
+    const query = gql`
+      query GetCategoryPost($slug: String!) {
+        postsConnection(where: {categories_some: {slug: $slug}}) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.postsConnection.edges;
+  };
+  
