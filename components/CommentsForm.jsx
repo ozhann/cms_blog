ac26d54d78b26883c48eb81fefe8899cda0e079e
ozhann <ozhanyilmaz@gmail.com>
2021-12-05 20:03:28 +0100

    add comment form component

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const CommentsForm = () => {
+import { submitComment } from '../services'
+
+const CommentsForm = ({ slug }) => {
+    const [error, setError] = useState(false)
+    const [localstorage, setLocalstorage] = useState(null)
+    const [showSuccessMessage, setShowsuccessMessage] = useState(false)
+    const commentEl = useRef()
+    const nameEl = useRef()
+    const emailEl = useRef()
+    const storeDataEl = useRef()
+
+    useEffect(() => {
+        nameEl.current.value = window.localStorage.getItem('name')
+        emailEl.current.value = window.localStorage.getItem('email')
+
+    }, [])
+
+    const handleCommentSubmission = () => {
+        setError(false)
+
+        const { value: comment } = commentEl.current
+        const { value: name } = nameEl.current
+        const { value: email } = emailEl.current
+        const { checked: storeData } = storeDataEl.current
+
+
+        if(!comment || !name || !email) {
+            setError(true)
+            return
+        }
+
+        const commentObj = { name, email, comment, slug }
+
+        if(storeData) {
+            window.localStorage.setItem('name', name)
+            window.localStorage.setItem('email', email)
+        } else {
+            window.localStorage.removeItem('name', name)
+            window.localStorage.removeItem('email', email)
+        }
+
+        submitComment(commentObj)
+        .then(res => {
+            setShowsuccessMessage(true)
+
+            setTimeout(() => {
+                setShowsuccessMessage(false)
+            }, 3000)
+        })
+    }
+
+
     return (
-        <div>
-            comments form
+        <div className='p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg'>
+            <h3 className="pb-4 mb-8 text-xl font-semibold border-b">Leave a comment</h3>
+            <div className="grid grid-cols-1 gap-4 mb-4">
+                <textarea 
+                ref={commentEl} 
+                className='w-full p-4 text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200'
+                placeholder='Comment'
+                name='comment'
+                />
+            </div>
+            <div className="grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2">
+                <input type='text' ref={nameEl}
+                className='w-full px-4 py-2 text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200'
+                placeholder='Name'
+                name='name'
+                />
+            <input type='text' ref={emailEl}
+                className='w-full px-4 py-2 text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200'
+                placeholder='Email'
+                name='email'
+                />
+            </div>
+            <div className="grid grid-cols-1 gap-4 mb-4">
+                <div>
+                    <input ref={storeDataEl} type='checkbox' id='storeData' name='storeData' value="true" />
+                    <label className='ml-2 text-gray-500 cursor-pointer' htmlFor='storeData'>Save my e-mail and name for next time i comment</label>
+                </div>
+            </div>
+
+            {error && <p className='text-xs text-red-500'>All fields are required.</p>}
+            <div className="mt-8">
+                <button 
+                    type='button' 
+                    onClick={handleCommentSubmission}
+                    className='inline-block px-8 py-3 text-lg text-white transition duration-500 bg-pink-600 rounded-full cursor-pointer ease hover:bg-indigo-900'
+                    >
+                        Post Comment
+                </button>
+                {showSuccessMessage && <span className='float-right mt-3 text-xl font-semibold text-green-500'>Comment submitted for review.</span>}
+            </div>
+
         </div>
     )
 }
